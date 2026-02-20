2896c7b45054ad567c28ba02bd36bc62c87b06aa
Sriram <sriram.bhagavath@hotmail.com.au>
2026-02-20 16:16:12 +1100

    Creating an Account with a wallet and xrp_amount

--- FirstXRPL.py
+++ FirstXRPL.py
@@ -1,6 +1,7 @@
 import xrpl
 from xrpl.clients import JsonRpcClient
 from xrpl.core.keypairs import generate_seed, derive_keypair
+from xrpl.account import get_balance
 from xrpl.wallet import Wallet
 from xrpl.wallet import generate_faucet_wallet
 from xrpl.transaction import autofill_and_sign, submit_and_wait
@@ -8,16 +9,28 @@ from xrpl.models.transactions import Payment
 from xrpl.utils import xrp_to_drops
 
 # Connecting to TestNet
+client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
 
-
-class WalletAccount():
+class XRPAccount():
     def __init__(self):
         self.wallet = Wallet.create()
         self.address = self.wallet.classic_address
         self.seed = self.wallet.seed
         self.private_key = self.wallet.private_key
         self.public_key = self.wallet.public_key
-        self.client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+        self.xrp_amount = get_balance(self.address, client) / 1,000,000
+    def send_xrp(self, xrp_amount, recieving_wallet):
+        payment = xrpl.models.transactions.Payment(
+            account=self.address,
+            amount=xrpl.utils.xrp_to_drops(int(xrp_amount)),
+            destination=recieving_wallet.address,
+        )
+        try:
+            response = xrpl.transaction.submit_and_wait(payment, client, self.wallet)
+        except xrpl.transaction.XRPLReliableSubmissionException as e:
+            response = f"Submit failed: {e}"
+        return response
+
 
 
 
