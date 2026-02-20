5055d08e06f5c3ee3bdae521c83b604cbc996da2
Sriram <sriram.bhagavath@hotmail.com.au>
2026-02-20 15:43:38 +1100

    Understanding how xrpl transactions work.

--- FirstXRPL.py
+++ FirstXRPL.py
@@ -1,24 +1,18 @@
 import xrpl
-from xrpl import *
 from xrpl.clients import JsonRpcClient
+from xrpl.core.keypairs import generate_seed, derive_keypair
+from xrpl.wallet import Wallet
 from xrpl.wallet import generate_faucet_wallet
+from xrpl.transaction import autofill_and_sign, submit_and_wait
 from xrpl.models.transactions import Payment
-from xrpl.transaction import submit_and_wait
+from xrpl.utils import xrp_to_drops
 
 # Connecting to TestNet
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
-# fund two test accounts (1,000 XRP each for free)
-wallet1 = generate_faucet_wallet(client)
-wallet2 = generate_faucet_wallet(client)
-print(f"Sender Address: {wallet1.classic_address}")
-print(f"Reciever Address: {wallet2.classic_address}")
-# preparing payment
-my_payment = Payment(account = wallet1.classic_address, amount = xrpl.utils.xrp_to_drops(10), destination = wallet2.classic_address)
-# sign and submit the transaction
-response = submit_and_wait(my_payment, client, wallet1)
 
-# results
-print(f"Transaction result: {response.result["meta"]["TransactionResult"]}")
+class WalletAccount():
+    def __init__(self):
+        self.seed = generate_seed()
 
 
 
