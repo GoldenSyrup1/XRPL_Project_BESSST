d01420ad5faa9acd33d05b5b57c397d4ac091862
Sriram <sriram.bhagavath@hotmail.com.au>
2026-02-20 16:08:10 +1100

    Understanding how xrpl transactions work.

--- FirstXRPL.py
+++ FirstXRPL.py
@@ -8,12 +8,16 @@ from xrpl.models.transactions import Payment
 from xrpl.utils import xrp_to_drops
 
 # Connecting to TestNet
-client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
+
 
 class WalletAccount():
     def __init__(self):
-        seed = generate_seed()
-        self.wallet = Wallet.from_seed(seed)
+        self.wallet = Wallet.create()
+        self.address = self.wallet.classic_address
+        self.seed = self.wallet.seed
+        self.private_key = self.wallet.private_key
+        self.public_key = self.wallet.public_key
+        self.client = JsonRpcClient("https://s.altnet.rippletest.net:51234")
 
 
 
